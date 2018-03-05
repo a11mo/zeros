890ab558c6c50b288f609f71b7c8ee791a8112aa
allmo <a11mo@ya.ru>
2018-03-05 16:50:31 +0300

    implementation1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var a, b, c;
+  var d = 0;
+  a =  Math.floor(Math.log(number)) / Math.floor(Math.log(5));
+  var c = 0;
+  for(var i=1; i<=a; i++){
+    b = Math.pow(5,i);
+    d = d + Math.floor(number/b);
+  }
+  return d;
 }
